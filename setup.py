93fc722c2c8ec89119a882653fd14b71ff0ca785
dovedevic <dovedevic@gmail.com>
2020-12-01 03:50:08 -0500

    added standard setup file for pypi

---
+++ setup.py
@@ -0,0 +1,76 @@
+from setuptools import setup
+import re
+
+requirements = []
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
+version = ''
+with open('ipyc/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('version is not set')
+
+if version.endswith(('a', 'b', 'rc')):
+    # append version identifier based on commit count
+    try:
+        import subprocess
+        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += out.decode('utf-8').strip()
+        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += '+g' + out.decode('utf-8').strip()
+    except Exception:
+        pass
+
+readme = ''
+with open('README.rst') as f:
+    readme = f.read()
+
+extras_require = {
+    'docs': [
+        'sphinx==1.8.5',
+        'sphinxcontrib_trio==1.1.1',
+        'sphinxcontrib-websupport',
+    ]
+}
+
+setup(name='IPyC',
+      author='dovedevic',
+      url='https://github.com/dovedevic/IPyC',
+      project_urls={
+        "Documentation": "https://ipyc.readthedocs.io/en/latest/",
+        "Issue tracker": "https://github.com/dovedevic/IPyC/issues",
+      },
+      version=version,
+      packages=['ipyc'],
+      license='GPL',
+      description='A basic IPC implementation for Python',
+      long_description=readme,
+      long_description_content_type="text/x-rst",
+      include_package_data=True,
+      install_requires=requirements,
+      extras_require=extras_require,
+      python_requires='>=3.5.3',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GPL License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+      ]
+)
