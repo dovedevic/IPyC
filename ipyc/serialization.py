485249c4f9fb94b99d54d58055e876f452ab9f8a
dovedevic <dovedevic@gmail.com>
2021-01-21 17:27:09 -0500

    added doc strings to the serialization and deserialization registers

--- ipyc/serialization.py
+++ ipyc/serialization.py
@@ -9,27 +9,82 @@ IPYC_CUSTOM_DESERIALIZATIONS = {
 }
 
 
-def add_custom_serialization(class_object, class_serializer):
+def add_custom_serialization(class_object: object, class_serializer):
+    """Register a serialization function for a particular object class. Only
+    one serialization method per object is allowed. The method must be a non-
+    blocking function.
+
+    Parameters
+    ------------
+    class_object: :class:`object`
+        The class or object type that will be serialized.
+    class_serializer: function
+        The function to call when serialization is requested for the object. Must return a string.
+
+    .. warning::
+        If a custom serializer is defined, ensure a deserializer is defined as well. These methods
+        could be @classmethod methods defined in the class for which you wish to transfer.
+    """
     IPYC_CUSTOM_SERIALIZATIONS[class_object.__name__] = class_serializer
 
 
-def update_custom_serialization(class_object, class_serializer):
+def update_custom_serialization(class_object: object, class_serializer):
+    """Wrapped method for :meth:`add_custom_serialization`"""
     add_custom_serialization(class_object, class_serializer)
 
 
-def remove_custom_serialization(class_object):
+def remove_custom_serialization(class_object: object):
+    """De-register a serialization function for a particular object class.
+
+    Parameters
+    ------------
+    class_object: :class:`object`
+        The class or object type that will be removed from the custom serializer dictionary.
+
+    .. warning::
+        If a custom serializer is removed, serialization of this object will then
+        default to python's builtins.
+    """
     if class_object.__name__ in IPYC_CUSTOM_SERIALIZATIONS:
         del IPYC_CUSTOM_SERIALIZATIONS[class_object.__name__]
 
 
-def add_custom_deserialization(class_object, class_deserializer):
+def add_custom_deserialization(class_object: object, class_deserializer):
+    """Register a deserialization function for a particular object class. Only
+    one deserialization method per object is allowed. The method must be a non-
+    blocking function.
+
+    Parameters
+    ------------
+    class_object: :class:`object`
+        The class or object type that will be serialized.
+    class_deserializer: function
+        The function to call when deserialization is requested for the object. Must return a :class:`object`.
+
+    .. warning::
+        If a custom deserializer is defined, ensure a serializer is defined as well, or that
+        the deserializer can evaluate builtin serializations. These methods could be
+        @classmethod methods defined in the class for which you wish to transfer.
+    """
     IPYC_CUSTOM_DESERIALIZATIONS[class_object.__name__] = class_deserializer
 
 
-def update_custom_deserialization(class_object, class_deserializer):
+def update_custom_deserialization(class_object: object, class_deserializer):
+    """Wrapped method for :meth:`add_custom_deserialization`"""
     add_custom_deserialization(class_object, class_deserializer)
 
 
-def remove_custom_deserialization(class_object):
+def remove_custom_deserialization(class_object: object):
+    """De-register a deserialization function for a particular object class.
+
+    Parameters
+    ------------
+    class_object: :class:`object`
+        The class or object type that will be removed from the custom deserializer dictionary.
+
+    .. warning::
+        If a custom deserializer is removed, deserialization of this object will then
+        default to python's builtins.
+    """
     if class_object.__name__ in IPYC_CUSTOM_DESERIALIZATIONS:
         del IPYC_CUSTOM_DESERIALIZATIONS[class_object.__name__]
